9ec822417449ccd3d1703b0fea8e4410ef2f0bdd
dipen70 <dipenbhattarai07@gmail.com>
2025-09-04 10:04:09 +0545

    submit

--- src/q4.c
+++ src/q4.c
@@ -1,8 +1,11 @@
-#include<stdio.h>
-int main(){
-    int i;
-    for (i=0;i<=100;i=i+2){
-        printf("%d\n",i);
+#include <stdio.h>
+int main() {
+    int i, sum = 0;
+
+    for (i = 2; i <= 50; i = i + 2) {  
+
+        printf("%d\n", i);                
     }
+
     return 0;
 }
