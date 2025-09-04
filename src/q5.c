9e7984364039937fd24c1b153c1f88d08e60da23
dipen70 <dipenbhattarai07@gmail.com>
2025-09-04 10:15:14 +0545

    message submit

--- src/q5.c
+++ src/q5.c
@@ -1,13 +1,21 @@
 #include<stdio.h>
 int main(){
-    int n,fact=1,i;
+    int n,fact=1,i=1;
     printf("enter a number: ");
     scanf("%d",&n); 
-    i=1;
+    if (n<0){
+        printf("Factorial is not defined for negative numbers.\n");
+    }
+    else if (n==0){
+        printf("factorial of %d is 1",n);
+    }
+else{      
     while (i<=n){
         fact = fact *i;
         i++;
     }
-    printf("Factorial of %d is %d\n",n,fact);
+        printf("Factorial of %d is %d\n",n,fact);
+    }
+
     return 0;
 }
